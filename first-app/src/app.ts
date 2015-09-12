53ab07d22ca467ace16a825dc072c12b0b078e4a
robertobsc <robertobsc@gmail.com>
2015-09-12 00:15:27 -0300

    primeiro app com angular2

---
+++ first-app/src/app.ts
@@ -0,0 +1,11 @@
+
+import {Component, View, bootstrap} from 'angular2/angular2';
+@Component({
+    selector: 'my-app'
+})
+@View({
+    template: '<h1 id="output">My second Angular 2 App</h1>'
+})
+class AppComponent { }
+
+bootstrap(AppComponent);
